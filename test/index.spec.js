9d60612dba7ba18cf74916e3a23e0c26237d345b
WalterCJ <waltercavalcantejunior@gmail.com>
2018-08-31 22:01:07 -0300

    add remove test

--- test/index.spec.js
+++ test/index.spec.js
@@ -18,6 +18,10 @@ describe('Merge Files', function () {
             const ShouldMergeFile = file.merge(path.join(dirname,'file_0'),path.join(dirname,'file_1'),'random title')
             expect(ShouldMergeFile).to.be.a('promise')
         })
+        it('Should remove files', async function () {
+            const result = await file.remove(path.join(dirname,'file_1'))
+            expect(result).to.be.true
+        })
     })
     after(function(done) {
         util.removeFolder()
