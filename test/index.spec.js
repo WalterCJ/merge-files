b12bdbaa3f8fec58679015702091f151d7fa19bc
WalterCJ <waltercavalcantejunior@gmail.com>
2018-08-31 21:07:09 -0300

    add first test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,11 @@
-var expect = require('chai').expect;
+const path = require('path')
+const file = require('../src/file')
+const expect = require('chai').expect;
+const chai = require("chai");
 const util = require('./util/handleFiles')
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
 
 describe('Merge Files', function () {
     before(function(done) { 
@@ -7,6 +13,14 @@ describe('Merge Files', function () {
         done()
     });
     describe('mergeFiles', function () {
-        it('')
+        const dirname = path.join(__dirname, '/util/files')   
+        it('Should return a promise', function () {   
+            const ShouldMergeFile = file.merge(path.join(dirname,'file_0'),path.join(dirname,'file_1'),'random title')
+            expect(ShouldMergeFile).to.be.a('promise')
+        })
     })
+    after(function(done) {
+        util.removeFolder()
+        done()
+    });
 });
