5af5dc1626b0f4b8084238a60bd1558856b35878
WalterCJ <waltercavalcantejunior@gmail.com>
2018-09-03 19:58:05 -0300

    organize tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -13,14 +13,19 @@ describe('Merge Files', function () {
         done()
     });
     describe('mergeFiles', function () {
-        const dirname = path.join(__dirname, '/util/files')   
-        it('Should return a promise', function () {   
-            const ShouldMergeFile = file.merge(path.join(dirname,'file_0'),path.join(dirname,'file_1'),'random title')
-            expect(ShouldMergeFile).to.be.a('promise')
+        describe('merge', function () {
+            const dirname = path.join(__dirname, '/util/files')   
+            it('Should return a promise', function () {   
+                const ShouldMergeFile = file.merge(path.join(dirname,'file_0'),path.join(dirname,'file_1'),'random title')
+                expect(ShouldMergeFile).to.be.a('promise')            
+            })
         })
-        it('Should remove files', async function () {
-            const result = await file.remove(path.join(dirname,'file_1'))
-            expect(result).to.be.true
+        describe('merge', function () {
+            const dirname = path.join(__dirname, '/util/files')   
+            it('Should remove files', async function () {
+                const result = await file.remove(path.join(dirname,'file_1'))
+                expect(result).to.be.true
+            })
         })
     })
     after(function(done) {
